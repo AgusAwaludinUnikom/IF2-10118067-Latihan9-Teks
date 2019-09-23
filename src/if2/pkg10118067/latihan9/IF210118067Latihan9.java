4d7ac1d30346b5f717d1b59ad723addd65b92db3
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-09-23 18:36:43 +0700

    Add files via upload

---
+++ src/if2/pkg10118067/latihan9/IF210118067Latihan9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118067.latihan9;
+
+/**
+ *
+ * @author
+ * Nama     : Agus Awaludin
+ * Kelas    : IF-2
+ * NIM      : 10118067
+ * Latihan 9. Teks
+ */
+public class IF210118067Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "berisi " + c);
+    }
+    
+}
